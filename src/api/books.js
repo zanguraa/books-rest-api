8e90fca0d62c031e20d694c2860df48bb150780a
Valeri Zanguri <vzanguri@credo.ge>
2023-09-26 18:04:16 +0400

    feat: added update and delete method

--- src/api/books.js
+++ src/api/books.js
@@ -28,5 +28,29 @@ export const getBooks = async () => {
 };
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const updateBook  = async (id, newTitle, newStart, newEnd) => {
+  const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: newTitle,
+      start: newStart,
+      end: newEnd,
+    }),
+  });
+  const status = await response.status;
+
+  return status;
+};
 
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+export const deleteBook = async (id) => {
+  const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+    method: "DELETE",
+  });
+  const status = await response.status;
+
+  return status;
+};
